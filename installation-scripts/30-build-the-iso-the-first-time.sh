79e878169695710222c36e06d111c9e3329b4b2d
Erik Dubois <erik.dubois@gmail.com>
2020-12-07 19:27:02 +0100

    new version for new calamares

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="spectrwm"
 	lightdmDesktop="spectrwm"
 
-	arcolinuxVersion='v21.01.2'
+	arcolinuxVersion='v21.01.3'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
