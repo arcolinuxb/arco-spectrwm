f553fdbef9c2921888b3a5722d206d499cc46fbb
Erik Dubois <erik.dubois@gmail.com>
2021-01-01 13:27:09 +0100

    new version for calamares config kernels and nvidia

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="spectrwm"
 	lightdmDesktop="spectrwm"
 
-	arcolinuxVersion='v21.01.6'
+	arcolinuxVersion='v21.01.7'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
