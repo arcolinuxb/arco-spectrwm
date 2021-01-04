75090cdeb10d11574cb8a7c652a73e02d5c0218e
Erik Dubois <erik.dubois@gmail.com>
2021-01-04 15:48:24 +0100

    version 21.01.8

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="spectrwm"
 	lightdmDesktop="spectrwm"
 
-	arcolinuxVersion='v21.01.7'
+	arcolinuxVersion='v21.01.8'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
