4a861141566264b76b3823bc402a67125aa85b0f
Erik Dubois <erik.dubois@gmail.com>
2021-03-27 13:54:28 +0100

    v21.05.1

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="spectrwm"
 	dmDesktop="spectrwm"
 
-	arcolinuxVersion='v21.03.11'
+	arcolinuxVersion='v21.05.1'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
